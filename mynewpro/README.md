5cab9877cac3fb44ca635a376296233c111a059e
changxinpengsdo <774903150@qq.com>
2020-08-24 17:51:17 +0800

    重构

--- mynewpro/README.md
+++ mynewpro/README.md
@@ -1,4 +1,4 @@
-# myownnewvue
+# mynewpro
 
 ## Project setup
 ```
--- mynewpro/src/main.js
+++ mynewpro/src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
-
-Vue.use(VueLazyLoad,{
-  preLoad: 1,
-  loading: require('assets/img/common/placeholder.png')
-})
+Vue.use(ElementUI)
 
 new Vue({
   router,
