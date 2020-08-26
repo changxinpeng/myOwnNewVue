48c8a6e9a38ba3d8883c0ad5da3d817b7724d1b9
changxinpengsdo <774903150@qq.com>
2020-08-26 17:45:12 +0800

    用elementUI重构1__底部菜单栏

---
+++ mynewpro/src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="tab-bar-item" @click="itemClick">
+    <div class="item-icon" v-show="!isActive"><slot name="icon"></slot></div>
+    <div class="item-active-icon" v-show="isActive"><slot name="active-icon"></slot></div>
+    <div class="item-text" :style="activeStyle"><slot name="text"></slot></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabBarItem",
+    components: {
+      
+    },
+    props: {
+      link: {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      isActive() {
+        return this.$route.path.indexOf(this.link) !== -1
+      },
+      activeStyle() {
+        return this.isActive ? {"color": "red"} : {}
+      }
+    },
+    methods: {
+      itemClick() {
+        console.log(this);
+        // this.$router.replace(this.link)
+      }
+    },
+  };
+</script>
+
+<style scoped>
+
+</style>
